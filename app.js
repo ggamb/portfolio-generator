8c99352809e7317901e6a7cfd2764d519ec40564
ggamb <glenngambardella372@gmail.com>
2021-09-28 10:32:57 -0400

    added html

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
-var commandLineArgs = process.argv;
+const fs = require('fs');
+const generatePage = require('./src/page-template');
 
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+const profileDataArgs = process.argv.slice(2);
 
-const printProfileData = profileDataArr => {
-    profileDataArr.forEach(profileItem => console.log(profileItem)); 
-}
+console.log(profileDataArgs);
 
-printProfileData(profileDataArgs);
+const [name, github] = profileDataArgs;
+
+console.log(name, github);
+
+const pageHTML = generatePage(name, github);
+
+fs.writeFile('./index.html', pageHTML, err => {
+  if (err) throw err;
+
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
