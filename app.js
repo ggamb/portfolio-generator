d0fa6e5a46c81373e05cf2ff65e056fa4332573c
ggamb <glenngambardella372@gmail.com>
2021-10-03 15:24:02 -0400

    added css stlye

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const inquirer = require("inquirer");
 const fs = require('fs');
 const generatePage = require('./src/page-template');
+const { writeFile, copyFile } = require('./utils/generate-site.js');;
 
 
 //const profileDataArgs = process.argv.slice(2);
@@ -59,14 +60,15 @@ const promptUser = () => {
 
 const promptProject = portfolioData => {
   console.log(`
-=================
-Add a New Project
-=================
-`);
+    =================
+    Add a New Project
+    =================
+  `);
 
-if(!portfolioData.projects) {
-  portfolioData.projects = [];
-}
+  if(!portfolioData.projects) {
+    portfolioData.projects = [];
+  }
+  
   return inquirer.prompt([
     {
       type: 'input',
@@ -136,21 +138,44 @@ if(!portfolioData.projects) {
   })
 };
 
-promptUser()
+/*promptUser()
 .then(promptProject)
 .then(portfolioData => {
    const pageHTML = generatePage(portfolioData);
 
-    fs.writeFile('./index.html', pageHTML, err => {
-      if (err) throw new Error(err);
-
-      console.log('Page created! Check out index.html in this directory to see it!');
+   fs.writeFile('./dist/index.html', pageHTML, err => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    console.log('Page created! Check out index.html in this directory to see it!');
+  
+    fs.copyFile('./src/style.css', './dist/style.css', err => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      console.log('Style sheet copied successfully!');
     });
-});
+  });
+});*/
 //const pageHTML = generatePage(name, github);
 
-/*fs.writeFile('./index.html', pageHTML, err => {
-  if (err) throw err;
-
-  console.log('Portfolio complete! Check out index.html to see the output!');
-});*/
+promptUser()
+  .then(promptProject)
+  .then(portfolioData => {
+    return generatePage(portfolioData);
+  })
+  .then(pageHTML => {
+    return writeFile(pageHTML);
+  })
+  .then(writeFileResponse => {
+    console.log(writeFileResponse);
+    return copyFile();
+  })
+  .then(copyFileResponse => {
+    console.log(copyFileResponse);
+  })
+  .catch(err => {
+    console.log(err);
+  });
