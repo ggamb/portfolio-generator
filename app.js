3b93c2695fbe5aa6c4fcc16f27f4c188bc37c150
ggamb <glenngambardella372@gmail.com>
2021-10-09 19:40:44 -0400

    minor fixes

--- app.js
+++ app.js
@@ -138,28 +138,6 @@ const promptProject = portfolioData => {
   })
 };
 
-/*promptUser()
-.then(promptProject)
-.then(portfolioData => {
-   const pageHTML = generatePage(portfolioData);
-
-   fs.writeFile('./dist/index.html', pageHTML, err => {
-    if (err) {
-      console.log(err);
-      return;
-    }
-    console.log('Page created! Check out index.html in this directory to see it!');
-  
-    fs.copyFile('./src/style.css', './dist/style.css', err => {
-      if (err) {
-        console.log(err);
-        return;
-      }
-      console.log('Style sheet copied successfully!');
-    });
-  });
-});*/
-//const pageHTML = generatePage(name, github);
 
 promptUser()
   .then(promptProject)
